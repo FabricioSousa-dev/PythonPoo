0675ded219786722167c99a1538c0ee11e228d91
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-08-17 15:37:41 -0300

    more

---
+++ ex001/ex001.py
@@ -0,0 +1,29 @@
+#Declaração da classe.
+class Gafanhoto:
+    def __init__(self): #Metado construtor
+        #Atributos de instância.
+        self.nome = ''
+        self.idade = 0
+
+    #Metodos de instância
+    def aniversario(self):
+        self.idade += 1
+    def mensagem(self):
+        return f"{self.nome} é gafanhoto tem {self.idade} anos"
+
+
+
+
+#Declaração do objetos.
+g1 = Gafanhoto()
+g1.nome = "Maria"
+g1.idade = 20
+g1.aniversario()
+print(g1.mensagem())
+
+
+g2 = Gafanhoto()
+g2.nome = "Julian"
+g2.idade = 23
+g2.aniversario()
+print(g2.mensagem())
