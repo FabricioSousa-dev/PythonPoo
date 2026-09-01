db5d5edf08172eef5f7298532d2ab22ef5c55f4c
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-09-01 15:36:20 -0300

    complete

--- exercicios/ex006/__main__.py
+++ exercicios/ex006/__main__.py
@@ -2,18 +2,23 @@ from rich import print, inspect
 from aluno import Aluno
 from funcionario import Funcionario
 from professor import Professor
-a1 = Aluno('josé',17, "Informática", "T01")
-a1.Aniversario()
-a1.Fazer_matricula()
-#inspect(a1,methods=True)
+
+def main():
+    a1 = Aluno('josé',17, "Informática", "T01")
+    a1.Aniversario()
+    a1.Fazer_matricula()
+    #inspect(a1,methods=True)
 
 
-p1 = Professor("Samuel", 37, "Biologia", "Mestre")
-p1.Aniversario()
-p1.Dar_aula()
-#inspect(p1,methods=True)
+    p1 = Professor("Samuel", 37, "Biologia", "Mestre")
+    p1.Aniversario()
+    p1.Dar_aula()
+    #inspect(p1,methods=True)
 
-f1 = Funcionario("Claudia", 27, "Secretária", "Secretaria")
-f1.Aniversario()
-f1.Bater_pontos()
-#inspect(f1,methods=True)
+    f1 = Funcionario("Claudia", 27, "Secretária", "Secretaria")
+    f1.Aniversario()
+    f1.Bater_pontos()
+    #inspect(f1,methods=True)
+
+if __name__ == '__main__':
+    main()
