2abaf9e3612284b6e6a002da39d3fd728d930bb8
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-09-08 16:00:35 -0300

    add rich funcions

--- rich/rich001.py
+++ rich/rich001.py
@@ -1,1 +1,4 @@
 from rich import print
+print("Olá , [red]mundo![/red] :earth_americas:")
+print("Olá, [bold blue on white]pequeno gafanhoto[/] :vulcon_salute: ")
+print("+1  -1 ")
---
+++ rich/rich002.py
@@ -0,0 +1,6 @@
+from rich import print, panel
+from rich.panel import Panel
+
+caixa = Panel("[white]Esse aqui é um exemplo[/]", title="Caixa", style="red")
+
+print(caixa)
